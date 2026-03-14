1921c6bca2b71ddc89f5dbf0a9400af9fe756b17
Soha <saha.20350436@science.aun.edu.eg>
2026-03-14 14:20:31 +0200

    Setting routes

---
+++ frontend/src/routes/AppRouter.jsx
@@ -0,0 +1,50 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { LoadingPage } from "../pages/LoginPage";
+import { RegisterPage } from "../pages/RegisterPage";
+import { LinksPage } from "../pages/LinksPage";
+import { DashboardPage } from "../pages/DashboardPage";
+import { ProtectedRoute } from "../routes/ProtectedRoute";
+import { GuestRoute } from "../routes/GuestRoute";
+
+export default function AppRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/login"
+          element={
+            <GuestRoute>
+              <LoadingPage />
+            </GuestRoute>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <GuestRoute>
+              <RegisterPage />
+            </GuestRoute>
+          }
+        />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <DashboardPage />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/links"
+          element={
+            <ProtectedRoute>
+              <LinksPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
