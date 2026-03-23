9774dfe7b61c2a636675885ec0d68f268f99e174
Soha <saha.20350436@science.aun.edu.eg>
2026-03-23 16:33:59 +0200

    Create form for adding new links

--- frontend/src/api/links.js
+++ frontend/src/api/links.js
@@ -4,10 +4,10 @@ export const getLinks = (params = {}) => {
     return api.get("/links", { params });
 };
 
-export const CreateLink = (data) => {
+export const createLink = (data) => {
     return api.post("/links", data);
 };
 
-export const DeleteLink = (id) => {
+export const deleteLink = (id) => {
     return api.delete(`/links/${id}`);
 };
--- frontend/src/pages/LinksPage.jsx
+++ frontend/src/pages/LinksPage.jsx
@@ -1,19 +1,19 @@
 import { useEffect, useState } from "react";
-import { getLinks } from "../api/links";
+import { getLinks, createLink } from "../api/links";
 
 export default function LinksPage() {
   const [links, setLinks] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
 
-  const fetchLinks = async () => {
-    setLoading(true);
-    setError("");
+  const [url, setUrl] = useState("");
+  const [creating, setCreating] = useState(false);
 
+  const fetchLinks = async () => {
     try {
       const res = await getLinks();
       setLinks(res.data.data);
-    } catch (err) {
+    } catch {
       setError("Failed to Load Links");
     } finally {
       setLoading(false);
@@ -24,12 +24,46 @@ export default function LinksPage() {
     fetchLinks();
   }, []);
 
+  const handleCreate = async (e) => {
+    e.preventDefault();
+
+    if (!url.trim()) return;
+
+    setCreating(true);
+
+    try {
+      const res = await createLink({ url });
+
+      setLinks((prev) => [res.data.data, ...prev]);
+      setUrl("");
+    } catch (err) {
+      console.log(err);
+      alert("Failed to create link");
+    } finally {
+      setCreating(false);
+    }
+  };
+
   if (loading) return <p>Loading links...</p>;
   if (error) return <p>{error}</p>;
 
   return (
     <div>
       <h1>My Links</h1>
+
+      <form onSubmit={handleCreate}>
+        <input
+          type="text"
+          placeholder="Paste your link..."
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+        />
+        <button type="submit" disabled={creating}>
+          {creating ? "Adding..." : "Add"}
+        </button>
+      </form>
+
+      {/* List */}
       {links.length === 0 && <p>No Links Yet</p>}
 
       <ul>
